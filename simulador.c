bff969afbeca9d58c6cd61d7e4fa68a8f5bb066a
Toothy <silviofgomesneto2@gmail.com>
2025-05-29 18:22:51 -0300

    Avancando na construcao do simulador

--- simulador.c
+++ simulador.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 
+//inicializa um simulador com paramatros passados
 void iniciar_simulador(Simulador *sim, int tam_pagina, int tam_memoria, int algoritmo){
     //Inicialização do simulador com os parametros passados
     sim->tamanho_pagina = tam_pagina;
@@ -26,5 +27,64 @@ void iniciar_simulador(Simulador *sim, int tam_pagina, int tam_memoria, int algo
     //lista de processos zerada
     sim->num_processos = 0;
     sim->processos = NULL;
+}
 
+void encerrar_simulador(Simulador *sim){
+    //liberar memoria da memoria física
+    free(sim->memoria.frames);
+    free(sim->memoria.tempo_carga);
+
+    //liberar memoria da tabela de paginas
+    for(int i = 0; i < sim->num_processos; i++){
+        free(sim->processos[i].tabela_paginas);
+    }
+
+    //Libera memoria dos processos
+    free(sim->processos);
+
+}
+
+//criar processo e adicina-lo ao simulador
+void criar_processo(Simulador *sim, int tam){
+    //inicializar um novo processo ('cacular pid, numero de paginas, alocar tabela de paginas, etc)
+    Processo novo_processo;
+    novo_processo.pid = sim->num_processos; // pid = indice
+    novo_processo.tamanho = tam;
+    novo_processo.num_paginas = (tam + sim->tamanho_pagina-1)/sim->tamanho_pagina; //divisao pra garantir que o processo todo caiba nas pags
+    novo_processo.tabela_paginas = (Pagina *) malloc(sizeof(Pagina)*novo_processo.num_paginas);
+
+    //inicializar tabela de paginas
+    for(int i = 0; i < novo_processo.num_paginas; i++){
+        novo_processo.tabela_paginas[i].presente = 0;
+        novo_processo.tabela_paginas[i].frame = -1;
+        novo_processo.tabela_paginas[i].modificada = 0;
+        novo_processo.tabela_paginas[i].referenciada = 0;
+        novo_processo.tabela_paginas[i].tempo_carga = 0;
+        novo_processo.tabela_paginas[i].ultimo_acesso = 0;
+    }
+
+    //alocar o processo no simulador
+    sim->processos = (Processo *)realloc(sim->processos, sizeof(Processo) * (sim->num_processos +1 ));
+    sim->processos[sim->num_processos] = novo_processo;
+    sim->num_processos++;
+
+    printf("Processo %d criado com %d paginas", novo_processo.pid, novo_processo.num_paginas);
+}
+
+//representação da memoria fisica
+void exibir_memoria(Simulador *sim){
+    printf("\nEstado da Memória Física:\n");
+    for(int i = 0; i < sim->memoria.num_frames; i++){
+        printf("--------\n");
+
+        int frame = sim->memoria.frames[i];
+        if(frame == -1){
+            printf("| --- |\n"); //frame vazio
+        }else{
+            int pid = frame >> 16;
+            int pagina = frame & 0xFFF;
+            printf("| P%-2d-%-2d |\n", pid, pagina);
+        }
+    }
+    printf("--------\n");
 }
